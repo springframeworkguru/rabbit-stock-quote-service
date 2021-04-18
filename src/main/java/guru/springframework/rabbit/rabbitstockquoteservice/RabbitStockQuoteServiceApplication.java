cb874c68a505287b80e467967659de4b61098730
John Thompson <john@springframework.guru>
2021-04-18 15:02:46 -0400

    initial project add

---
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/RabbitStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.rabbit.rabbitstockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitStockQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitStockQuoteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/rabbit/rabbitstockquoteservice/RabbitStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.rabbit.rabbitstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
