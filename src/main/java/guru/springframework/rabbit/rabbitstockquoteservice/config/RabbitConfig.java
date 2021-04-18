7c549567a07080a1e76e39ff5c47eb8d6a549232
John Thompson <john@springframework.guru>
2021-04-18 15:26:02 -0400

    adding sender config

---
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/config/RabbitConfig.java
@@ -0,0 +1,39 @@
+package guru.springframework.rabbit.rabbitstockquoteservice.config;
+
+import com.rabbitmq.client.Connection;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+import reactor.rabbitmq.RabbitFlux;
+import reactor.rabbitmq.Sender;
+import reactor.rabbitmq.SenderOptions;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * Created by jt on 4/18/21.
+ */
+@Configuration
+public class RabbitConfig {
+    public static final String QUEUE = "quotes";
+
+    @Autowired
+    Mono<Connection> connectionMono;
+
+    @Bean
+    Mono<Connection> connectionMono(CachingConnectionFactory connectionFactory){
+        return Mono.fromCallable(() -> connectionFactory.getRabbitConnectionFactory().newConnection());
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        connectionMono.block().close();
+    }
+
+    @Bean
+    Sender sender(Mono<Connection> mono) {
+        return RabbitFlux.createSender(new SenderOptions().connectionMono(mono));
+    }
+}
